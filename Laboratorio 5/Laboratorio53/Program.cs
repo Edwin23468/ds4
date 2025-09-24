58e1ff57ac38952de0ea962c062173f63b65f7df
Edwin Cisneros <edwincisneros2346@gmail.com>
2025-09-23 20:08:46 -0500

    LAB 5 COMPLETADO

---
+++ Laboratorio 5/Laboratorio53/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class program
+{
+    private static void Main(string[] args)
+    {
+        string[] frutas = { "manzana", "plátano", "naranja" };
+
+        foreach (string fruta in frutas)
+        {
+            Console.WriteLine(fruta);
+        }
+    }
+}
---
+++ Laboratorio 5/Laboratorio54/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Crear una lista de calificaciones
+        List<int> calificaciones = new List<int> { 85, 90, 78, 92, 88 };
+
+    int suma = 0;
+
+foreach (int calificacion in calificaciones)
+{
+    suma += calificacion;
+
+
+    double promedio = suma / (double)calificaciones.Count;
+
+    Console.WriteLine($"El promedio de las calificaciones es: {promedio}");
+            }
+    }
+}
